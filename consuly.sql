fa4df3b341b54617fa6eedc5e8d347d4ea13b760
Andreys-Gp <109758319+Andreys-Gp@users.noreply.github.com>
2023-10-07 22:40:45 -0500

    reload

---
+++ consuly.sql
@@ -0,0 +1,16 @@
+USE jugos;
+
+INSERT INTO tbproductos(
+producto, nombre, envase, volumen, sabor,
+precio) VALUES ('695594', 'Festival de Sabores', 'Botella PET',
+'1.5 Litros', 'Asaí', 18.51);
+
+INSERT INTO tbproductos(
+producto, nombre, envase, volumen, sabor,
+precio) VALUES ('1041119', 'Línea Citrus', 'Botella de Vidrio',
+'700 ml', 'Lima', 4.90);
+
+UPDATE tbproductos SET Nombre='Clean'
+WHERE volumen='700 ml';
+
+SELECT * FROM tbproductos;
