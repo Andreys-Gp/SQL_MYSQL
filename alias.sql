67a2cd529ae1853ba87281966fbae92c374381c8
Andreys-Gp <109758319+Andreys-Gp@users.noreply.github.com>
2023-10-30 20:10:39 -0500

    Update alias.sql

--- alias.sql
+++ alias.sql
@@ -1,3 +1,9 @@
 USE jugos_ventas;
 /*SELECT DNI, NOMBRE FROM tabla_de_clientes*/
 SELECT DNI AS IDENTIICACION, NOMBRE AS CLIENTE FROM tabla_de_clientes;
+
+SELECT * FROM tabla_de_productos WHERE SABOR='Uva';
+
+/* entre intervalos */
+SELECT * FROM tabla_de_productos WHERE PRECIO_DE_LISTA BETWEEN 16 AND 16.02;
+
