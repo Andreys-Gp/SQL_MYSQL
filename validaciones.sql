560d1e38d021889dba026a551df2d97fa96cafa1
Andreys-Gp <109758319+Andreys-Gp@users.noreply.github.com>
2024-01-03 12:03:20 -0500

    Update validaciones.sql

--- validaciones.sql
+++ validaciones.sql
@@ -12,5 +12,24 @@ select * from tabla_de_productos limit 5,3;
 
 /** filtrando consulta ORDER BY **/
 select * from tabla_de_productos;
-select PRECIO_DE_LISTA, TAMANO from tabla_de_productos order by PRECIO_DE_LISTA asc, TAMANO desc
+select PRECIO_DE_LISTA, TAMANO from tabla_de_productos order by PRECIO_DE_LISTA asc, TAMANO desc;
 
+/** filtrando con GROUP BY sum() max() min() avg() promedio count()**/
+select * from tabla_de_productos;
+select * from tabla_de_clientes;
+select ESTADO, sum(LIMITE_DE_CREDITO) as LIMITE_TOTAL from tabla_de_clientes group by ESTADO;
+select * from tabla_de_clientes;
+select CIUDAD, BARRIO , sum(LIMITE_DE_CREDITO) as LIMITE_TOTAL from tabla_de_clientes where CIUDAD='ciudad de mexico' group by BARRIO;
+
+/** filtrando datos con HAVING **/ 
+ select CIUDAD, BARRIO , sum(LIMITE_DE_CREDITO) as LIMITE_TOTAL from tabla_de_clientes group by BARRIO
+ having LIMITE_TOTAL>70000;
+ 
+ select ENVASE , max(PRECIO_DE_LISTA) as PRECIO_MAXIMO, min(PRECIO_DE_LISTA) as PRECIO_MINIMO 
+   from tabla_de_productos group by ENVASE;
+   
+select ENVASE, max(PRECIO_DE_LISTA) as PRECIO_MAXIMO, min(PRECIO_DE_LISTA) as PRECIO_MAXIMO
+ from tabla_de_productos group by ENVASE having sum(PRECIO_DE_LISTA)>80;
+ 
+ 
+ 
