58a76b6eb04701e010e6d1e30744e681493dbed6
Brayan-Andreys <109758319+Andreys-Gp@users.noreply.github.com>
2024-01-22 21:58:58 -0500

    Update jugosVentas.sql

--- jugosVentas.sql
+++ jugosVentas.sql
@@ -1,3 +1,13 @@
 use jugos_ventas;
 use ventas_jugos;
 select * from jugos_ventas.tabla_de_productos;
+
+USE ventas_jugos;
+
+INSERT INTO tb_clientes
+SELECT DNI, NOMBRE, DIRECCION_1 AS DIRECCION,
+BARRIO, CIUDAD, ESTADO, CP, FECHA_DE_NACIMIENTO 
+AS FECHA_NACIMIENTO, EDAD, SEXO, LIMITE_DE_CREDITO
+AS LIMITE_CREDITO, VOLUMEN_DE_COMPRA AS VOLUMEN_COMPRA,
+PRIMERA_COMPRA FROM jugos_ventas.tabla_de_clientes
+WHERE DNI NOT IN (SELECT DNI FROM tb_clientes);
